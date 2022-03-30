81567b2930e5150fd3bcefba07747373a0173d3f
leo corpuz <leocorpuz.jr27@gmail.com>
2022-03-30 17:43:51 +0800

    sorry bro puro ako commit hahahhaa

--- user/login.php
+++ user/login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../style/authentication_style.css?v=1">
+    <link rel="stylesheet" href="../style/authentication_style.css?v=2">
 </head>
 <body>
    
