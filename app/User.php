9d250ee8160fefc6f7c6d7654e3827552e7a13c5
William Galas <williamgalas2@gmail.com>
2022-03-31 00:46:12 +0800

    Create User.php

---
+++ app/User.php
@@ -0,0 +1,42 @@
+<?php
+namespace App;
+
+class User
+{
+    const TYPE_MANAGER = 'Manager';
+    const TYPE_TRAVELER = 'Traveler';
+    const TYPE_ADMIN = 'Admin';
+
+    public static function createUser($type, $data)
+    {
+        //please write query here
+        if ($type === self::TYPE_ADMIN) {
+            //write code below to process the creation of admin
+
+            return true;
+        }
+
+        if ($type === self::TYPE_MANAGER) {
+            //write code below to process the creation of manager
+
+            return true;
+        }
+
+        if ($type === self::TYPE_TRAVELER) {
+            //write code below to process the creation of traveler
+
+            return true;
+        }
+
+        //return false, if the process was failure.
+        return false;
+    }
+
+    public function getFullName() // this function should return the full name of the user.
+    {
+        //return concatinated first, middle and last name.
+        $str = ""; // edit this code
+        return $str;
+    }
+
+}
