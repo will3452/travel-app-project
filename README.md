25121c3df3a1edb0988708075dcd88f90193547f
William Galas <williamgalas2@gmail.com>
2022-03-31 10:58:20 +0800

    wip

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # travel-app-project
 
-PHP * HTML * CSS only
+## Installation
+  1. download and install composer
+  2. goto to the root folder of the project
+  3. open terminal and run composer install
+  4. to create database thru script, open browser and browse all scripts under "database" folder
--- classes/User.php
+++ classes/User.php
@@ -7,36 +7,17 @@ class User extends Eloquent
     const TYPE_TRAVELER = 'Traveler';
     const TYPE_ADMIN = 'Admin';
 
-    public static function createUser($type, $data)
-    {
-        //please write query here
-        if ($type === self::TYPE_ADMIN) {
-            //write code below to process the creation of admin
-
-            return true;
-        }
-
-        if ($type === self::TYPE_MANAGER) {
-            //write code below to process the creation of manager
-
-            return true;
-        }
-
-        if ($type === self::TYPE_TRAVELER) {
-            //write code below to process the creation of traveler
-
-            return true;
-        }
-
-        //return false, if the process was failure.
-        return false;
-    }
-
-    public function getFullName() // this function should return the full name of the user.
-    {
-        //return concatinated first, middle and last name.
-        $str = ""; // edit this code
-        return $str;
-    }
-
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+        'phone',
+        'email',
+        'password',
+        'type',
+        'manager_type',
+        'image',
+        'status',
+        'subscribed_at'
+    ];
 }
