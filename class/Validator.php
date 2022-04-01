8716d1fa63f7d41dcdaa532ef9191994b75a565d
William Galas <williamgalas2@gmail.com>
2022-04-01 20:06:28 +0800

    simplify Validator class

--- class/Validator.php
+++ class/Validator.php
@@ -1,105 +1,101 @@
 <?php
 
 
-class Validator{
+class Validator
+{
 
-    public function ValidatePassword($password){   //validate Password
-        $passwordVal = $this->password=$password;
-        $uppercase = preg_match('@[A-Z]@', $passwordVal);
-        $lowercase = preg_match('@[a-z]@', $passwordVal);
-        $number    = preg_match('@[0-9]@', $passwordVal);
-        $specialChars = preg_match('@[^\W_]*[\W_]@', $passwordVal);
-        if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 10){
-            return false;
-        }else{
-            return true;
-        }
+    const VALID_MANAGER_TYPE = [
+        "Resort manager",
+        "Bed and breakfast manager",
+        "Rental vehicle manager",
+        "tourist attraction manager",
+        "Resto and cafe manager",
+    ];
+
+
+    public function ValidatePassword($password)  //validate Password
+    {
+        $uppercase = preg_match('@[A-Z]@', $password);
+        $lowercase = preg_match('@[a-z]@', $password);
+        $number = preg_match('@[0-9]@', $password);
+        $specialChars = preg_match('@[^\W_]*[\W_]@', $password);
+        return $uppercase && $lowercase && $number && $specialChars && (strlen($password) < 10 );
     }
-    public function ValidateSignUpForm($first_name, $middle_name, $last_name, $email, $phone, $password, $manager_type){
-        if(empty($this->first_name=$first_name) || empty($this->middle_name=$middle_name) || empty($this->last_name=$last_name)
-            || empty($this->email=$email) || empty($this->phone=$phone) || empty($this->password=$password) ||
-            empty($this->manager_type=$manager_type)){
-            return false;
-        }else{
-            return true;
-        }
-    }
-    public function ValidateLogin($email, $password){
-        if(empty($this->email=$email) || empty($this->password=$password)){
-            return false;
-        }else{
-            return true;
-        }
-    }
-    public function ValidateContact($phone){
-        $phoneVal = $this->phone=$phone;
-        if(!is_numeric($phoneVal)){
-            return false;
-        }else{
-           return true;
-        }
-    }
-    public function ValidateEmail($email){
-        $emailVal = $this->email=$email;
-        if(!filter_var($emailVal, FILTER_VALIDATE_EMAIL)){
-            return false;
-        }else{
-           return true;
-        }
-    }
-    public function ValidateToken($token){
-        $token = $this->token=$token;
-        if(empty($token)){
-            return false;
-        }else{
-            date_default_timezone_set('Asia/Manila');
-            $hashvalue = date("Y-m-d").'token-ps'; //security code
-            if(password_verify($hashvalue,$token)){
-                return true;
-            }else{
+
+    public function ValidateSignUpForm( ...$args)
+    {
+
+        foreach ($args as $arg) {
+            if (empty($arg)) {
                 return false;
             }
         }
+        return true;
     }
-    public function ValidateImage($file){
-        $fileimages = $this->file=$file;
-        $filename = $this->file=$file['name'];
-        $filesize = $this->file=$file['size'];
-        $fileerror = $this->file=$file['error'];
-        $profilename = $this->file=$file['name'];
-        $filetype = $this->file=$file['type'];
 
-        $fileext = explode('.',$filename);
+    public function ValidateLogin($email, $password)
+    {
+        return $this->ValidateSignUpForm($email, $password);
+    }
+
+    public function ValidateContact($phone)
+    {
+        return is_numeric($phone);
+    }
+
+    public function ValidateEmail($email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    public function ValidateToken($token)
+    {
+        if ($this->ValidateSignUpForm($token)) {
+
+            date_default_timezone_set('Asia/Manila');
+
+            $date = date("Y-m-d");
+
+            $hashvalue = $date."token-ps";
+
+            return password_verify($hashvalue, $token);
+        }
+
+        return false;
+    }
+
+    public function ValidateImage($file)
+    {
+        $filename = $file['name'];
+        $filesize = $file['size'];
+        $fileerror = $file['error'];
+
+        $fileext = explode('.', $filename);
         $fileactex = strtolower(end($fileext));
 
-        $allowed = array('jpg','jpeg','png'); 
-        if(empty($filename)){
+        $allowed = array('jpg','jpeg','png');
+
+        if (empty($filename)) {
             return "emp";
-        }else{
-            if(!in_array($fileactex, $allowed)){
-                return "fileexterror";
-            }else{
-                if(!$fileerror===0){
-                    return "fileeror";
-                }else{
-                    if($filesize>15000000){
-                        return "fileoversize";
-                    }else{
-                        return true;
-                    }
-                }
-            }
         }
+
+        if (! in_array($fileactex, $allowed)) {
+            return "fileexterror";
+        }
+
+        if (! $fileerror === 0) {
+            return "fileeror";
+        }
+
+        if ($filesize > 15000000) {
+            return "fileoversize";
+        }
+
+        return true;
     }
-    public function ManagerTypeValidator($manager_type){
-        $manager_arr = array("Resort manager", "Bed and breakfast manager", "Rental vehicle manager", "tourist attraction manager", "Resto and cafe manager");
 
-        if (in_array($this->manager_type=$manager_type, $manager_arr))
-        {
-            return true;
-        }else{
-            return false;
-        }
-
+    public function ManagerTypeValidator($manager_type)
+    {
+        return in_array($manager_type, self::VALID_MANAGER_TYPE);
     }
 }
