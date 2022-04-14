8d0a92aaec0bd6db0e4f8c7944a7ee30a76eb149
William Galas <williamgalas2@gmail.com>
2022-04-14 23:56:03 +0800

    wip

--- class/example.Connection.php
+++ class/example.Connection.php
@@ -1,17 +1,17 @@
 <?php
 class Connection{
     private $servername = "127.0.0.1";
-    private $username = "root";
+    private $username = "";
     private $password = "";
-    private $dbname = "travel_guide";
+    private $dbname = "";
 
     public function con(){
         try {
 
           $this->pdo = new PDO("mysql:host=$this->servername;port='';dbname=$this->dbname", $this->username, $this->password);
-        
+
         } catch(PDOException $e) {
-            
+
             die($e->getMessage());
         }
         return $this->pdo;
