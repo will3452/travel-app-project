dd1464abb4c2f6b19907a1b9f7c37df47083068d
leo27-corpuz <65518382+leo27-corpuz@users.noreply.github.com>
2022-05-18 14:30:12 +0800

    update

--- PHPmailersetup/mailsetup.php
+++ PHPmailersetup/mailsetup.php
@@ -18,30 +18,14 @@
 
                 
     $mail->isSMTP();   
-    $email = "admin@ostg.online";
+    $email = "libutasugbo.admn@gmail.com";
     $name = "Travel Guide for Cebu Province Inc.";                                         
-    $mail->Host       = 'smtp.hostinger.com';                 
+    $mail->Host       = 'smtp.gmail.com';                 
     $mail->SMTPAuth   = true;                                 
     $mail->Username   = $email;                   
-    $mail->Password   = '#Qwerty123456';                     
+    $mail->Password   = 'iexplorers@123';                     
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;           
     $mail->Port       = 465;  
     $mail->SMTPSecure = "ssl";
     $mail->isHTML(true);
     $mail->setFrom($email, $name);
-
-    // $mail->isSMTP();   
-    // $email = "juncorpuz941@gmail.com";
-    // $name = "Travel Guide for Cebu Province Inc.";                                         
-    // $mail->Host       = 'smtp.gmail.com';                 
-    // $mail->SMTPAuth   = true;                                 
-    // $mail->Username   = $email;                   
-    // $mail->Password   = 'Petvetgocorp_14';                     
-    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;           
-    // $mail->Port       = 465;  
-    // $mail->SMTPSecure = "ssl";
-    // $mail->isHTML(true);
-    // $mail->setFrom($email, $name);
-
-// email setting
-    
